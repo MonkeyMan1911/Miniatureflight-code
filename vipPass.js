da0c4e8df474b0e19c5b77f852f16bb77cc8b7db
MonkeyMan1911 <sahil@guptainc.net>
2025-06-26 14:20:07 -0400

    Update vipPass.js

--- vipPass.js
+++ vipPass.js
@@ -2,10 +2,17 @@ let coords = api.getPosition(myId);
 
 if (!api.hasItem(myId, "Black Carpet")) {
 	if (api.getInventoryItemAmount(myId, "Diamond") >= 5) {
-	api.createItemDrop(coords[0], coords[1], coords[2], "Black Carpet", 1, false, {customDisplayName: "VIP Ticket"});
-	api.sendMessage(myId, "Welcome to the VIP Club!", {color: "lightgreen"});
+		api.createItemDrop(coords[0], coords[1], coords[2], "Black Carpet", 1, false, {customDisplayName: "VIP Ticket"});
+		api.sendMessage(myId, "Welcome to the VIP Club!", {color: "lightgreen"});
+		api.removeItemName(myId, "Diamond", 5);
+
 	}
 	else {
 		api.sendMessage(myId, "You need " + (5 - api.getInventoryItemAmount(myId, "Diamond")) + " more diamonds to enter.", {color: "red"});
 	}
 }
+else {
+	api.sendMessage(myId, "You already have a VIP Ticket", {color: "red"});
+	api.setPosition(myId, x, y, z); // Replace x, y, z, with coordinates of VIP club
+}
+
